b162a0cc1ad8467e2e27a78aac014a665d335d9e
vaibhav5697 <vaibhavwarpade101@gmail.com>
2025-05-23 17:25:21 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : ''database-1.cjiuoucaecep.ap-south-1.rds.amazonaws.com,
     DB_USER : 'admin',
-    DB_PWD : 'vaibhav123',
+    DB_PWD : 'Vaibhav123',
     DB_DATABASE : 'database-1'
 });
